8126c18f53f082cebaeafcdb9dce4157b024851d
eyi1 <ej7729@gmail.com>
2018-01-29 02:27:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,40 @@
+var katzDeli = [];
+
+function takeANumber (katzDeliLine, name){
+
+katzDeliLine.push(name)
+
+return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
+}
+
+takeANumber(katzDeli, 'Ada');
+takeANumber(katzDeli, 'Grace');
+takeANumber(katzDeli, 'Kent');
+
+var deliLine = [
+  "Steven",
+  "Blake",
+  "Avi"]
+
+function nowServing (katzDeliLine){
+ katzDeliLine.shift([0])
+  if (katzDeliLine.length === 0){
+  return "There is nobody waiting to be served!";
+}else{
+  return "Currently serving " + deliLine[0] + "."
+}
+ }
+ nowServing([]);
+
+
+
+function currentLine(katzDeliLine){
+
+     if (katzDeliLine.length === 0){
+      return "The line is currently empty."
+    }else{
+       return(`The line is currently:` + katzDeliLine);
+
+    }
+}
+ currentLine(["Bill"," Jane"," Ann"])
